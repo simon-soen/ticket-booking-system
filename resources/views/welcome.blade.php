6c89172eed4ed11bb685e5ea68425f898a6f1069
simon-soen <in160005620@kisiiuniversity.ac.ke>
2024-04-12 17:27:25 +0300

    ux

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,8 +6,21 @@
         <div class="col-md-8 text-center">
             <h1 class="display-4 mt-5 mb-4">Welcome to Our Events Platform</h1>
             <p class="lead mb-5">Join our community and explore a world of exciting events happening near you.</p>
-            <a href="{{ route('events.index') }}" class="btn btn-primary btn-lg">Show Available Events</a>
+            <div class="mb-3">
+                <a href="{{ route('events.index') }}" class="btn btn-primary btn-lg btn-block">Show Available Events</a>
+            </div>
+            <div>
+                <a href="{{ route('admin.events.index') }}" class="btn btn-secondary btn-lg btn-block">Create Event</a>
+            </div>
         </div>
     </div>
 </div>
+
+<style>
+    .btn-block {
+        display: block;
+        width: 100%;
+        margin-bottom: 10px; 
+    }
+</style>
 @endsection
